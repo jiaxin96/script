a26fb1464f64a5e6fc09734f116161e016547b69
jiaxin96 <572673807@qq.com>
2017-08-16 22:42:26 +0800

    图片转扫描办

--- 批量图片转扫描图片/code/getWBImg.py
+++ 批量图片转扫描图片/code/getWBImg.py
@@ -19,18 +19,28 @@ from PIL import Image
 import os
 import os.path
 
-for parent, dirnames, filenames in os.walk("../data/"):
+for parent, dirnames, filenames in os.walk("../data"):
     for filename in filenames:
         im = Image.open("../data/" + filename)
         im = im.convert("P")
+
+        if (im.size[0] > im.size[1]):
+            im = im.transpose(Image.ROTATE_270)
         im2 = Image.new("P",im.size,255)
-
-
+        print(im.size[1])
+        l = im.size[1] / 2;
+        w = im.size[0] / 2;
         for x in range(im.size[1]):
             for y in range(im.size[0]):
                 pix = im.getpixel((y,x))
-                if abs(pix)<116:
-                    im2.putpixel((y,x),0)
+                # if abs(pix)<105:
+                #     im2.putpixel((y,x),0)
+                if x > l:
+                    if abs(pix)<120+(40/((2*w)**2))*y*(w*2-y):
+                        im2.putpixel((y,x),0)
+                else:
+                    if abs(pix)<125:
+                        im2.putpixel((y,x),0)
 
         im2.convert('RGB').save('../ans/'+filename)
         im.close()
